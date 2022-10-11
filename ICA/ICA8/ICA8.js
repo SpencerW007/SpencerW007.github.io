415be401bc72583c8bff2bfb4ddeb52d11a6dbae
SpencerW007 <spwi1807@colorado.edu>
2022-10-11 12:03:21 -0600

    Updates to ICA8

---
+++ ICA/ICA8/ICA8.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body");
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  body.style.backgroundColor = 'blue';
+});
+
+btn.addEventListener('click', function onClick(event) {
+   body.style.color = 'white';});
