22ba8907fe0c02cfe5994f2f00e8a8724247c4b2
SpencerW007 <spwi1807@colorado.edu>
2022-10-24 22:38:40 -0600

    Quiz 10 update

--- Assignments/Assignment9/main.js
+++ Assignments/Assignment9/main.js
@@ -9,11 +9,11 @@ const overlay = document.querySelector('.overlay');
 
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'pic1.jpg' : 'Ronaldo',
+  'pic2.jpg' : 'Messi',
+  'pic3.jpg' : 'Neymar',
+  'pic4.jpg' : 'Kevin De Bruyne',
+  'pic5.jpg' : 'Son Heung-min'
 }
 
 /* Looping through images */
